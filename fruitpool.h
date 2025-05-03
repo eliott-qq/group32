72cacbec101b9ba490b4908f45860726abc0210a
eliott-qq <u3629144@connect.hku.hk>
2025-05-03 19:52:21 +0800

    Update fruitpool.h

--- fruitpool.h
+++ fruitpool.h
@@ -1,1 +1,11 @@
+#ifndef FRUITPOOL_H
+#define FRUITPOOL_H
 
+#include <vector>
+#include <string>
+#include <map>
+
+extern std::vector<std::string> fruitNames;
+extern std::map<std::string, std::string> fruitColors;
+
+#endif
