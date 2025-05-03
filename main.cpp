37ce3cff11433f47e6d283f6a69b2a1f7e0a1c38
IrishB1 <u3632637@connect.hku.hk>
2025-05-03 22:07:51 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include "game.h"
+#include "utils.h"
+#include <iostream>
+using namespace std;
+
+// Game Introduction
+
+int main() {
+    clearScreen();
+    loadRecords(recordHead);
+    cout << "Welcome to the Memory Puzzle Game!" << endl;
+
+// Screen Display Options: Starting Game or Resuming Game
+
+    do {
+        bool loadSaved = false;
+        if (hasSavedGame()) {
+            char choice;
+            while (true) {
+                cout << "\nDo you want to:\n";
+                cout << "  N - Start a new game\n";
+                cout << "  R - Resume saved game\n";
+                cout << "Enter N or R: ";
+                cin >> choice;
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                choice = toupper(choice);
+                cout << endl;
+                if (choice == 'R') {
+                    loadSaved = true;
+                    break;
+                } else if (choice == 'N') {
+                    break;
+                } else {
+                    cout << "Invalid choice. Please enter N or R.\n";
+                }
+            }
+        }
+        runProgram(loadSaved); // Game Function Initiated
+    } while (shouldRestart());
+
+    clearScreen();
+    cout << "Program ended." << endl;
+    return 0;
+}
